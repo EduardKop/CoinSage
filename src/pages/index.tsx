0b5864b378d7012cd2eeefaa868e83b910c04554
Eduard <eduard.korytnyk@gmail.com>
2023-10-20 14:55:29 +0300

    add charts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Coin from '../components/Coin';
 import ToggleThemeButton from '@/components/ToggleThemeButton';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+
+
 
 interface CoinData {
   name: string;
@@ -12,16 +16,25 @@ interface CoinData {
   changePercent24Hr: string;
   marketCapUsd: string;
 }
+interface HistoryData {
+  date: string;
+  priceUsd:string;
+  name:string;
+  price:any;
+}
+
 
 export default function Page() {
   const [coins, setCoins] = useState<CoinData[]>([]);
   const [search, setSearch] = useState<string>('');
   const [infoCoin, setInfoCoin] = useState<CoinData | null>();
-  const [historyInfo, sethistoryInfo] = useState();
+  const [historyInfo, sethistoryInfo] = useState<HistoryData[]>([]);
+  const [chartData, setchartData] = useState<HistoryData[]>([]);
 
   useEffect(() => {
     if (infoCoin) {
-    
+      console.log(infoCoin)
+
     axios.get(`https://api.coincap.io/v2/assets/${(infoCoin.name).toLowerCase()}/history?interval=d1`)
     .then((res) => {
         // sethistoryInfo(res.data.data);
@@ -30,12 +43,12 @@ export default function Page() {
         result.push(res.data.data[res.data.data.length-i])
       }
         sethistoryInfo(result);
-        
+      
       })
       .catch((error) => console.log(error));
     }
-  }, historyInfo);
-  
+  }, [infoCoin]);
+
   useEffect(() => {
     axios
       .get('https://api.coincap.io/v2/assets/')
@@ -58,9 +71,76 @@ export default function Page() {
     const result = coins.find((target) => target.name === e);
     if (result) {
       setInfoCoin(result);
-      console.log(historyInfo)
+      let arr: HistoryData[] = [];
+      let historyInfo_length = historyInfo.length;
+  
+      for (let i = 0; i < historyInfo_length; i++) {
+          try {
+            if(historyInfo[i].priceUsd != undefined){
+            let roundedPriceUsd = parseFloat(historyInfo[i].priceUsd).toFixed(3);
+            let dateStr = historyInfo[i].date.split("T")[0];
+            
+            arr.push({
+                name: dateStr,
+                price: roundedPriceUsd,
+            });
+          }
+          }catch(err){
+          console.log(err)
+          }
+          
+       
+        
+      }
+      console.log(arr)
+      setchartData(arr);
     }
   };
+  const fetchHistoryData = (coinName: string) => {
+    axios.get(`https://api.coincap.io/v2/assets/${coinName.toLowerCase()}/history?interval=d1`)
+      .then((res) => {
+        let result: any = [];
+        for (let i = 1; i < 7; i++) {
+          result.push(res.data.data[res.data.data.length - i]);
+        }
+        sethistoryInfo(result);
+
+        // Добавлено обработка данных для графика
+        let arr: HistoryData[] = [];
+        for (let i = 0; i < result.length; i++) {
+          if (result[i].priceUsd != undefined) {
+            let roundedPriceUsd = parseFloat(result[i].priceUsd).toFixed(3);
+            let dateStr = result[i].date.split("T")[0];
+            arr.push({
+              name: dateStr,
+              price: roundedPriceUsd,
+            });
+          }
+        }
+        setchartData(arr);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    if (infoCoin) {
+      fetchHistoryData(infoCoin.name);
+    }
+  }, [infoCoin]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.coincap.io/v2/assets/')
+      .then((res) => {
+        setCoins(res.data.data);
+        const bitcoin = res.data.data.find((coin: any) => coin.name === 'Bitcoin');
+        if (bitcoin) {
+          setInfoCoin(bitcoin);
+          fetchHistoryData(bitcoin.name);  // Загружаем исторические данные для Bitcoin при первой загрузке
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   const filteredCoins: CoinData[] = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
@@ -124,6 +204,15 @@ export default function Page() {
   </div>
 )}
                 </span>
+              </div>
+              <div className={styles.carts}>
+              <LineChart width={600} height={300} data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="price" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                </LineChart>
               </div>
             </div>
           )}
